0c093400b3f175f9b23cd95466c9aadd1f6b2286
zjupple <zjfontenot@gmail.com>
2023-11-02 14:08:19 -0700

    latest update but no CSS

---
+++ Unit-27-React-Effects-and-Refs-master/src/App.js
@@ -0,0 +1,17 @@
+import DeckOfCards from './DeckOfCards';
+import './App.css';
+
+// import needed for other videocode
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <DeckOfCards />
+      
+    </div>
+  );
+}
+
+export default App;
